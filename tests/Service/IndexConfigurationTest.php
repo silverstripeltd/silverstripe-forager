e89906bb9e69fb9008da92aefe7da9577e98bb38
Bernie Hamlin <bernie@silverstripe.com>
2025-01-30 09:18:29 +1300

    add test for index validation

--- tests/Service/IndexConfigurationTest.php
+++ tests/Service/IndexConfigurationTest.php
@@ -8,6 +8,7 @@ use SilverStripe\Dev\SapphireTest;
 use SilverStripe\Forager\DataObject\DataObjectDocument;
 use SilverStripe\Forager\Schema\Field;
 use SilverStripe\Forager\Service\IndexConfiguration;
+use SilverStripe\Forager\Exception\IndexConfigurationException;
 use SilverStripe\Forager\Tests\Fake\DataObjectFake;
 use SilverStripe\Forager\Tests\Fake\DataObjectFakeAlternate;
 use SilverStripe\Forager\Tests\Fake\DataObjectSubclassFake;
@@ -352,6 +353,35 @@ class IndexConfigurationTest extends SapphireTest
         $this->assertContains($config->getSourceClassField(), $names);
         $this->assertContains(DataObjectDocument::config()->get('base_class_field'), $names);
         $this->assertContains(DataObjectDocument::config()->get('record_id_field'), $names);
+    }
+
+    public function testIndexConfigurationValidation(): void
+    {
+        $this->bootstrapIndexes();
+        $className = ServiceFake::class;
+
+        Config::modify()->merge(
+            IndexConfiguration::class,
+            'indexes',
+            [
+                'index5' => [
+                    'includeClasses' => [
+                        $className => [
+                            'fields' => [
+                                'field10' => [
+                                    'type' => 'invalid'
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        );
+
+        $config = IndexConfiguration::singleton();
+        $this->expectException(IndexConfigurationException::class);
+        $result = $config->getFieldsForIndex('index5');
+
     }
 
     protected function bootstrapIndexes(): void
