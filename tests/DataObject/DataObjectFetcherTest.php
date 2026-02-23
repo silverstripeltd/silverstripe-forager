d55a8221cf4adafbd21bd7d2f21879fbf44beac0
adunn49 <andrew.dunn@silverstripe.com>
2026-02-24 08:07:19 +1300

    Reinstate deleted unit test

--- tests/DataObject/DataObjectFetcherTest.php
+++ tests/DataObject/DataObjectFetcherTest.php
@@ -38,9 +38,43 @@ class DataObjectFetcherTest extends SearchServiceTest
         Subsite::class,
     ];
 
+    public function testFetch(): void
+    {
+        $fetcher = DataObjectFetcher::create(DataObjectFake::class);
+        /** @var DataObjectDocument[] $documents */
+        $documents = $fetcher->fetch();
+
+        // Quick sanity check to make sure we have the correct number of documents
+        $this->assertCount(3, $documents);
+
+        // Now start building out a basic $expectedDocuments, which is just going to be a combination of the ClassName
+        // and Title
+        $expectedDocuments = [
+            sprintf('%s-Dataobject one', DataObjectFake::class),
+            sprintf('%s-Dataobject two', DataObjectFake::class),
+            sprintf('%s-Dataobject three', DataObjectFake::class),
+        ];
+
+        $resultDocuments = [];
+
+        foreach ($documents as $document) {
+            // We expect each of our Documents to be a DataObjectDocument specifically
+            $this->assertInstanceOf(DataObjectDocument::class, $document);
+
+            // And now let's start collating our ClassName/Title data
+            $resultDocuments[] = sprintf('%s-%s', $document->getSourceClass(), $document->getDataObject()?->Title);
+        }
+
+        $this->assertEqualsCanonicalizing($expectedDocuments, $resultDocuments);
+
+        // And then just a quick extra sanity check that fetching 2 Documents only returns 2 Documents
+        $documents = $fetcher->fetch(2);
+
+        $this->assertCount(2, $documents);
+    }
 
     /**
-     * This tests that we can fetch all documents when processed in batches.
+     * This tests that we fetch all documents when processed in batches.
      */
     public function testFetchBatch(): void
     {
